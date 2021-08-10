c779b7e4f893c8cc169f0abcf0e421cab6727208
eatmyvenom <eat.my.venomm@gmail.com>
2021-08-09 20:34:41 -0700

    change project indentation

--- Logger.js
+++ Logger.js
@@ -1,5 +1,5 @@
 const {
-    argv
+  argv
 } = require("process");
 let name = argv[2];
 name = name == "bot" ? argv[argv.length - 1] : name;
@@ -9,8 +9,8 @@ name = name == undefined ? "hyarcade" : name;
  * @returns {string} Formatted time
  */
 function daytime () {
-    const d = new Date();
-    return `${(`0${d.getMonth()}`).slice(-2)}/${(`0${d.getDate()}`).slice(-2)}-${(`0${d.getHours()}`).slice(-2)}:${(`0${d.getMinutes()}`).slice(-2)}:${(`0${d.getSeconds()}`).slice(-2)}:${(`00${d.getMilliseconds()}`).slice(-3)}`;
+  const d = new Date();
+  return `${(`0${d.getMonth()}`).slice(-2)}/${(`0${d.getDate()}`).slice(-2)}-${(`0${d.getHours()}`).slice(-2)}:${(`0${d.getMinutes()}`).slice(-2)}:${(`0${d.getSeconds()}`).slice(-2)}:${(`00${d.getMilliseconds()}`).slice(-3)}`;
 }
 
 /**
@@ -19,9 +19,9 @@ function daytime () {
  * @param {string} color
  */
 function print (type, string, color = "\x1b[0m") {
-    for(const s of string?.toString()?.split("\n") ?? "") {
-        println(type, s, color);
-    }
+  for(const s of string?.toString()?.split("\n") ?? "") {
+    println(type, s, color);
+  }
 }
 
 /**
@@ -30,42 +30,42 @@ function print (type, string, color = "\x1b[0m") {
  * @param {string} color
  */
 function println (type, string, color = "\x1b[0m") {
-    const str = `[\x1b[36m${daytime().trim()}\x1b[0m] [\x1b[36m${name.trim()}\x1b[0m] [${color}${type}\x1b[0m]${color} ${string}\x1b[0m`;
-    console.log(str);
-    require("fs").writeFile("stdout.log", `${str}\n`, {
-        flag: "a"
-    }, () => {});
+  const str = `[\x1b[36m${daytime().trim()}\x1b[0m] [\x1b[36m${name.trim()}\x1b[0m] [${color}${type}\x1b[0m]${color} ${string}\x1b[0m`;
+  console.log(str);
+  require("fs").writeFile("stdout.log", `${str}\n`, {
+    flag: "a"
+  }, () => {});
 }
 
 /**
  * @param {string} string
  */
 function error (string) {
-    for(const s of string?.toString()?.split("\n") ?? "") {
-        errorln(s);
-    }
+  for(const s of string?.toString()?.split("\n") ?? "") {
+    errorln(s);
+  }
 }
 
 /**
  * @param {string} string
  */
 function errorln (string) {
-    const str = `[\x1b[36m${daytime().trim()}\x1b[0m] [\x1b[36m${name.trim()}\x1b[0m] [\x1b[31mERROR\x1b[0m]\x1b[31m ${string}\x1b[0m`;
-    console.log(str);
-    require("fs").writeFile("stderr.log", `${str}\n`, {
-        flag: "a"
-    }, () => {});
+  const str = `[\x1b[36m${daytime().trim()}\x1b[0m] [\x1b[36m${name.trim()}\x1b[0m] [\x1b[31mERROR\x1b[0m]\x1b[31m ${string}\x1b[0m`;
+  console.log(str);
+  require("fs").writeFile("stderr.log", `${str}\n`, {
+    flag: "a"
+  }, () => {});
 }
 
 module.exports = class Logger {
-    /**
-     * Log content to stdout or a file
-     *
-     * @param {string[]} content
-     */
-    static log (...content) {
-        print("LOG", content.join(" "));
-    }
+  /**
+   * Log content to stdout or a file
+   *
+   * @param {string[]} content
+   */
+  static log (...content) {
+    print("LOG", content.join(" "));
+  }
 
     static out = this.log;
 
@@ -75,7 +75,7 @@ module.exports = class Logger {
      * @param {string} content
      */
     static info (...content) {
-        print("INFO", content.join(" "), "\x1b[32m");
+      print("INFO", content.join(" "), "\x1b[32m");
     }
 
     /**
@@ -84,7 +84,7 @@ module.exports = class Logger {
      * @param {string} content
      */
     static warn (...content) {
-        print("WARN", content.join(" "), "\x1b[33m");
+      print("WARN", content.join(" "), "\x1b[33m");
     }
 
     /**
@@ -93,7 +93,7 @@ module.exports = class Logger {
      * @param {string} content
      */
     static debug (...content) {
-        print("DEBUG", content.join(" "), "\x1b[95m");
+      print("DEBUG", content.join(" "), "\x1b[95m");
     }
 
     static dbg = this.debug;
@@ -104,7 +104,7 @@ module.exports = class Logger {
      * @param {string} content
      */
     static error (...content) {
-        error(content.join(" "));
+      error(content.join(" "));
     }
 
     static err = this.error;
