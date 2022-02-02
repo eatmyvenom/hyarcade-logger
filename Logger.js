ae52da6622b5a87cee7d683857084ebe3cbc6612
eatmyvenom <eat.my.venomm@gmail.com>
2022-02-01 19:50:47 -0800

    style(lint): :sparkles: update repo tools
    Updates most tools used in the repo.
    NPM -> yarn
    eslint -> eslint+prettier
    -> husky
    -> commitlint
    
    NPM is no longer used

--- Logger.js
+++ Logger.js
@@ -1,53 +1,46 @@
 /* eslint-disable no-undef */
-const { writeFile } = require("fs-extra");
 const path = require("path");
-const {
-  argv,
-  stdout,
-  stderr
-} = require("process");
+const { argv, stdout, stderr } = require("process");
+const { writeFile } = require("fs-extra");
 
 let logLevel = 5;
-if(argv.includes("--verbose") || argv.includes("-v")) {
+if (argv.includes("--verbose") || argv.includes("-v")) {
   logLevel = 6;
 }
 
-const levels = [
-  "ERROR",
-  "WARN",
-  "LOG",
-  "INFO",
-  "DEBUG",
-  "VERBOSE"
-];
+const levels = ["ERROR", "WARN", "LOG", "INFO", "DEBUG", "VERBOSE"];
 
 /**
  * @param {string} type
  * @returns {boolean}
  */
-function shouldLog (type) {
+function shouldLog(type) {
   return levels.slice(0, logLevel).includes(type);
 }
 
 /**
  * @returns {string} Formatted time
  */
-function daytime () {
+function daytime() {
   const d = new Date();
-  return `${(`0${d.getMonth() + 1}`).slice(-2)}/${(`0${d.getDate()}`).slice(-2)}-${(`0${d.getHours()}`).slice(-2)}:${(`0${d.getMinutes()}`).slice(-2)}:${(`0${d.getSeconds()}`).slice(-2)}:${(`00${d.getMilliseconds()}`).slice(-3)}`;
+  return `${`0${d.getMonth() + 1}`.slice(-2)}/${`0${d.getDate()}`.slice(-2)}-${`0${d.getHours()}`.slice(
+    -2,
+  )}:${`0${d.getMinutes()}`.slice(-2)}:${`0${d.getSeconds()}`.slice(-2)}:${`00${d.getMilliseconds()}`.slice(-3)}`;
 }
 
 /**
  * @param {string} string
  * @param {string} name
  */
-function errorln (string, name) {
-  if(shouldLog("ERROR")) {
+function errorln(string, name) {
+  if (shouldLog("ERROR")) {
     const str = `[\x1b[36m${daytime().trim()}\x1b[0m] [\x1b[36m${name.trim()}\x1b[0m] [\x1b[31mERROR\x1b[0m]\x1b[31m ${string}\x1b[0m\n`;
     stderr.write(str, () => {});
   }
 
-  writeFile(path.join(process.cwd(), "logs", `${name.trim()}-err.log`), `${daytime().trim()} - ${string}\n`, { flag: "a" })
+  writeFile(path.join(process.cwd(), "logs", `${name.trim()}-err.log`), `${daytime().trim()} - ${string}\n`, {
+    flag: "a",
+  })
     .then(() => {})
     .catch(console.error);
 }
@@ -58,14 +51,15 @@ function errorln (string, name) {
  * @param {string} name
  * @param {string} color
  */
-function println (type, string, name, color = "\x1b[0m") {
-
-  if(shouldLog(type)) {
+function println(type, string, name, color = "\x1b[0m") {
+  if (shouldLog(type)) {
     const str = `[\x1b[36m${daytime().trim()}\x1b[0m] [\x1b[36m${name.trim()}\x1b[0m] [${color}${type}\x1b[0m]${color} ${string}\x1b[0m\n`;
     stdout.write(str, () => {});
   }
 
-  writeFile(path.join(process.cwd(), "logs", `${name.trim()}-out.log`), `${daytime().trim()} - ${string}\n`, { flag: "a" })
+  writeFile(path.join(process.cwd(), "logs", `${name.trim()}-out.log`), `${daytime().trim()} - ${string}\n`, {
+    flag: "a",
+  })
     .then(() => {})
     .catch(console.error);
 }
@@ -76,8 +70,8 @@ function println (type, string, name, color = "\x1b[0m") {
  * @param {string} name
  * @param {string} color
  */
-function print (type, string, name, color = "\x1b[0m") {
-  for(const s of string?.toString()?.split("\n") ?? "") {
+function print(type, string, name, color = "\x1b[0m") {
+  for (const s of string?.toString()?.split("\n") ?? "") {
     println(type, s, name, color);
   }
 }
@@ -86,14 +80,13 @@ function print (type, string, name, color = "\x1b[0m") {
  * @param {string} string
  * @param {string} name
  */
-function error (string, name) {
-  for(const s of string?.toString()?.split("\n") ?? "") {
+function error(string, name) {
+  for (const s of string?.toString()?.split("\n") ?? "") {
     errorln(s, name);
   }
 }
 
 module.exports = class Logger {
-
   static name;
 
   /**
@@ -101,8 +94,8 @@ module.exports = class Logger {
    *
    * @param {string[]} content
    */
-  static log (...content) {
-    if(Logger.name == undefined) {
+  static log(...content) {
+    if (Logger.name == undefined) {
       Logger.name = argv[2];
       Logger.name = Logger.name == "bot" ? argv[argv.length - 1] : Logger.name;
       Logger.name = Logger.name == undefined ? "hyarcade" : Logger.name;
@@ -111,85 +104,84 @@ module.exports = class Logger {
     print("LOG", content.join(" "), Logger.name);
   }
 
-    static out = this.log;
+  static out = this.log;
 
-    /**
-     * Log content to stdout or a file
-     *
-     * @param {string} content
-     */
-    static info (...content) {
-      if(Logger.name == undefined) {
-        Logger.name = argv[2];
-        Logger.name = Logger.name == "bot" ? argv[argv.length - 1] : Logger.name;
-        Logger.name = Logger.name == undefined ? "hyarcade" : Logger.name;
-      }
-
-      print("INFO", content.join(" "), Logger.name, "\x1b[32m");
+  /**
+   * Log content to stdout or a file
+   *
+   * @param {string} content
+   */
+  static info(...content) {
+    if (Logger.name == undefined) {
+      Logger.name = argv[2];
+      Logger.name = Logger.name == "bot" ? argv[argv.length - 1] : Logger.name;
+      Logger.name = Logger.name == undefined ? "hyarcade" : Logger.name;
     }
 
-    /**
-     * Log content to stdout or a file
-     *
-     * @param {string} content
-     */
-    static warn (...content) {
-      if(Logger.name == undefined) {
-        Logger.name = argv[2];
-        Logger.name = Logger.name == "bot" ? argv[argv.length - 1] : Logger.name;
-        Logger.name = Logger.name == undefined ? "hyarcade" : Logger.name;
-      }
+    print("INFO", content.join(" "), Logger.name, "\x1b[32m");
+  }
 
-      print("WARN", content.join(" "), Logger.name, "\x1b[33m");
+  /**
+   * Log content to stdout or a file
+   *
+   * @param {string} content
+   */
+  static warn(...content) {
+    if (Logger.name == undefined) {
+      Logger.name = argv[2];
+      Logger.name = Logger.name == "bot" ? argv[argv.length - 1] : Logger.name;
+      Logger.name = Logger.name == undefined ? "hyarcade" : Logger.name;
     }
 
-    /**
-     * Log content to stdout or a file
-     *
-     * @param {string} content
-     */
-    static debug (...content) {
-      if(Logger.name == undefined) {
-        Logger.name = argv[2];
-        Logger.name = Logger.name == "bot" ? argv[argv.length - 1] : Logger.name;
-        Logger.name = Logger.name == undefined ? "hyarcade" : Logger.name;
-      }
+    print("WARN", content.join(" "), Logger.name, "\x1b[33m");
+  }
 
-      print("DEBUG", content.join(" "), Logger.name, "\x1b[95m");
+  /**
+   * Log content to stdout or a file
+   *
+   * @param {string} content
+   */
+  static debug(...content) {
+    if (Logger.name == undefined) {
+      Logger.name = argv[2];
+      Logger.name = Logger.name == "bot" ? argv[argv.length - 1] : Logger.name;
+      Logger.name = Logger.name == undefined ? "hyarcade" : Logger.name;
     }
 
-    static dbg = this.debug;
+    print("DEBUG", content.join(" "), Logger.name, "\x1b[95m");
+  }
 
-    /**
-     * Log content to stdout or a file
-     *
-     * @param {string} content
-     */
-    static verbose (...content) {
-      if(Logger.name == undefined) {
-        Logger.name = argv[2];
-        Logger.name = Logger.name == "bot" ? argv[argv.length - 1] : Logger.name;
-        Logger.name = Logger.name == undefined ? "hyarcade" : Logger.name;
-      }
+  static dbg = this.debug;
 
-      
-      print("VERBOSE", content.join(" "), Logger.name, "\x1b[90m");
+  /**
+   * Log content to stdout or a file
+   *
+   * @param {string} content
+   */
+  static verbose(...content) {
+    if (Logger.name == undefined) {
+      Logger.name = argv[2];
+      Logger.name = Logger.name == "bot" ? argv[argv.length - 1] : Logger.name;
+      Logger.name = Logger.name == undefined ? "hyarcade" : Logger.name;
     }
 
-    /**
-     * Log content to stderr or a file
-     *
-     * @param {string} content
-     */
-    static error (...content) {
-      if(Logger.name == undefined) {
-        Logger.name = argv[2];
-        Logger.name = Logger.name == "bot" ? argv[argv.length - 1] : Logger.name;
-        Logger.name = Logger.name == undefined ? "hyarcade" : Logger.name;
-      }
+    print("VERBOSE", content.join(" "), Logger.name, "\x1b[90m");
+  }
 
-      error(content.join(" "), Logger.name);
+  /**
+   * Log content to stderr or a file
+   *
+   * @param {string} content
+   */
+  static error(...content) {
+    if (Logger.name == undefined) {
+      Logger.name = argv[2];
+      Logger.name = Logger.name == "bot" ? argv[argv.length - 1] : Logger.name;
+      Logger.name = Logger.name == undefined ? "hyarcade" : Logger.name;
     }
 
-    static err = this.error;
+    error(content.join(" "), Logger.name);
+  }
+
+  static err = this.error;
 };
